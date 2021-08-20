bf0a793bea74ab14faf022ca7266b5bed6cb8f9f
kev <kbreto2911@gmail.com>
2021-08-20 17:32:45 -0400

    index

--- next-money-app/components/container.js
+++ next-money-app/components/container.js
@@ -1,9 +1,10 @@
 import Navegation from "./navegation"
 import styles from '../styles/container.module.css'
 
-export default function Container({children}) {
+export default function Container({children, id, className}) {
+
     return (
-        <div id={styles['main-container']} >
+        <div  id={id ? id : styles['main-container']} className={className} >
             <Navegation />
             { children }
         </div>
--- next-money-app/pages/index.js
+++ next-money-app/pages/index.js
@@ -1,11 +1,28 @@
 import Container from "../components/container";
 
-const Index = () => {
+import styles from "../styles/index.module.css";
+// import playstoreIcon from "../assets/playstore-icon.png";
 
+const Index = () => {
+    const playstoreIcon = require("../assets/playstore-icon.png");
 
     return(
         <Container>
-            HOME
+            <div id={styles['main-container']}>
+                <section id={styles["introduction-sec"]}>
+                    <p id={styles["welcome-text"]}>Bienvenidos a WhitePay</p>
+                    <h1 id={styles["description-text"]}>La mejor app para enviar y <span className={styles["text-secondary-color"]}>recibir dinero de manera Facil</span></h1>
+                    <p id={styles["small-text"]}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium sint nemo ipsum impedit aliquid quas dolorem voluptatum fugiat</p>
+                    <button id={styles["play-store-button"]}>
+                        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d0/Google_Play_Arrow_logo.svg/512px-Google_Play_Arrow_logo.svg.png" alt="playstore icon" id={styles["playstore-icon"]} />
+                        <p>Play Store</p>
+                    </button>
+                </section>
+                <section id={styles["decor-sec"]} >
+                    <div id={styles["rectangle"]}>
+                    </div>
+                </section>
+            </div>
         </Container>
     )
 }
